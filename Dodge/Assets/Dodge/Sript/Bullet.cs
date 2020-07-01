f09cc1606a6ff3d31435f23be4897a72ed37bc74
Leeseungwoo <dizody@naver.com>
2020-07-01 21:22:56 +0900

    2020 0701

---
+++ Dodge/Assets/Dodge/Sript/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public int m_speed = 3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * m_speed * Time.deltaTime;
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.AddForce(transform.forward * 1.2f);
+    }
+}
