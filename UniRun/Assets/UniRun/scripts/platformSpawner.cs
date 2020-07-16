56bb5be7e54cb827c9dbb5acd9559b4159a3437b
Leeseungwoo <dizody@naver.com>
2020-07-16 20:50:17 +0900

    유니런 4일차

--- UniRun/Assets/UniRun/scripts/platformSpawner.cs
+++ UniRun/Assets/UniRun/scripts/platformSpawner.cs
@@ -2,46 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformSpawner : MonoBehaviour
+public class platformSpawner : MonoBehaviour
 {
-    public GameObject m_PlatformPrefab;
+    public GameObject m_platformspawner;
     public GameObject[] m_Platforms;
 
     public float m_MinY = -2f;
     public float m_MaxY = 2f;
 
-    public float m_MinDelay = 1.5f;
-    public float m_MaxDelay = 2.5f;
+    public float m_MinDelay = 3f;
+    public float m_MaxDelay = 5f;
 
-    public float m_SpawnDelay;
-    public int m_PlatformCount = 0;
+    public float m_SpawnDelay = 0;
+    public int m_PlatformCout = 0;
 
 
-    void Start()
+    public void Start()
     {
-        m_Platforms = new GameObject[3];
-        for(int i=0; i<3; i++)
+        m_PlatformCout = new GameObject[3];
+        for (int i = 0; i < 3; i++) ;
         {
-            m_Platforms[i] = GameObject.Instantiate(m_PlatformPrefab);
-            m_Platforms[i].SetActive(false);
+            m_Platforms[1] = GameObject.Instantiate(m_PlatformPrefab);
+            m_Platforms[1].SetActive(false);
+
         }
+
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if(m_SpawnDelay <= 0)
+        if (m_SpawnDelay <= 0) ;
         {
-            m_Platforms[m_PlatformCount].gameObject.SetActive(false);
-            m_Platforms[m_PlatformCount].SetActive(true);
+            m_Platforms[m_PlatformCout].SetActive(false);
+            m_Platforms[m_PlatformCout].SetActive(true);
 
             Vector2 spawnPosition = new Vector2(12, Random.Range(m_MinY, m_MaxY));
-            m_Platforms[m_PlatformCount].transform.position = spawnPosition;
+            m_Platforms[m_platformCount].transform.position = spawnPosition;
 
-            m_PlatformCount = (m_PlatformCount + 1) % 3;  //% 나머지 연산자 . 
+            m_PlatformCout = (m_PlatformCout + 1) % 3; //%나머지 연산자.
+
+            m_SpawnDelay=Random.Range(m_SpawnDelay-MinDelay)
 
 
-            m_SpawnDelay = Random.Range(m_MinDelay, m_MaxDelay);
         }
-        m_SpawnDelay -= Time.deltaTime;
     }
 }
